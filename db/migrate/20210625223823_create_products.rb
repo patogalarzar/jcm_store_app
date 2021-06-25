d5cfa8084ccdf68dd79119f68798b5f168b90703
patogalarzar <patricio.galarza.r@gmail.com>
2021-06-25 17:44:47 -0500

    “Scaffolds.”

---
+++ db/migrate/20210625223823_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :color
+      t.string :size
+      t.decimal :price
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
